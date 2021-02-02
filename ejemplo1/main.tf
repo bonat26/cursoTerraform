7930f17252db7b524751ef8705d27cd5b64feb62
Ubuntu <ubuntu@ip-172-31-5-144.eu-west-1.compute.internal>
2021-02-02 18:52:44 +0000

    dia 2

---
+++ ejemplo1/main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+        }
+    }
+}
+
+provider "docker" {}
+
+resource "docker_image" "imagen-ubuntu" {
+    name = "${var.nombre_imagen}:${var.version_imagen}"
+}
+
+resource "docker_container" "contenedor-ubuntu" {
+    name  = "mi_contenedor_ubuntu"
+    image = docker_image.imagen-ubuntu.latest
+    command = ["bash", "-c", "sleep 600"]
+
+    dynamic "volumes" {
+        for_each = var.volumenes
+        content {
+            volume_name     = contains(keys(volumes),"volume_name") ? volumes.value["volume_name"] : "volumen"
+            host_path       = volumes.value["host_path"] 
+            container_path  = volumes.value["container_path"]
+        }
+        
+    }
+}
